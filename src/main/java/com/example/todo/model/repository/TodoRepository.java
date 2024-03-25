4372ffa9fcadf9f3ec4726987e4459aaaf26bec0
Tim Kelly <63365669+timotheekelly@users.noreply.github.com>
2024-03-25 16:32:23 +0000

    refactor

--- src/main/java/com/example/todo/model/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.todo.model.repository;
-
-import com.example.todo.model.Todo;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface TodoRepository extends MongoRepository<Todo, String> {
-}
